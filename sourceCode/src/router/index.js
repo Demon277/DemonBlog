29b81fe401b8a7a56ad4e35988bb1e9d79df6602
杨泽红 <zhyang10@iflytek.com>
2019-09-05 20:32:34 +0800

    feat: add sourceCode

---
+++ sourceCode/src/router/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by bingoogolapple on 2017/7/14.
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
+
+Vue.use(Router)
+
+const router = new Router({
+  // mode: 'history',
+  routes: routes
+})
+
+export default router
